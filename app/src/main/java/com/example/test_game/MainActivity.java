7106d4b144a8cf0ef463fd32609551076a19d9dc
Monte-Kristo <83087001+RATTER95292@users.noreply.github.com>
2022-12-02 10:20:29 +0300

    Initial commit

---
+++ app/src/main/java/com/example/test_game/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.test_game;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
